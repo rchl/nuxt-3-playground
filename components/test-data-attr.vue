181f05479ef3f93342749123ee12d1e976277962
Rafal Chlodnicki <rchl2k@gmail.com>
2023-08-28 15:16:53 +0200

    test-data-attr

---
+++ components/test-data-attr.vue
@@ -0,0 +1,17 @@
+<template>
+    <div data-foo="x">
+        <emits data-foo="x" @close="closed = true" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+            closed: false,
+        }
+    },
+})
+</script>
